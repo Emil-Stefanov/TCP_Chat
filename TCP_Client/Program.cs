1482d509de176850e647e712686ec196e94bfb37
Emil Stefanov <101264480+Emil-Stefanov@users.noreply.github.com>
2024-09-26 13:46:59 +0300

    Update Program.cs

--- TCP_Client/Program.cs
+++ TCP_Client/Program.cs
@@ -5,12 +5,12 @@ using System.Threading;
 
 class Client
 {
-    private const int Port = 8888;
-    private const string ServerIp = "127.0.0.1";
+    private const int Port = 8888; // създава константна целочислена променлива относно порта със стойност 8888
+    private const string ServerIp = "127.0.0.1"; // създава константна променлива тип низ, съдържаща 4-те октети на примерен логически IP адрес
 
     static void Main()
     {
-        TcpClient client = new TcpClient(ServerIp, Port);
+        TcpClient client = new TcpClient(ServerIp, Port); // създава обект client от клас TcpClient с двете променливи - IP aдрес и номер на порта
         Console.WriteLine("Connected to server. Start chatting!");
 
         NetworkStream stream = client.GetStream();
@@ -30,7 +30,7 @@ class Client
     {
         NetworkStream stream = (NetworkStream)obj;
         byte[] buffer = new byte[1024];
-        int bytesRead;
+        int bytesRead; // инициализира целочислена променлива отностно прочетените байтове
 
         while (true)
         {
