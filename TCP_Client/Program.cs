f0f934a360ac417b892f68149800fa233ac14833
Emil Stefanov <101264480+Emil-Stefanov@users.noreply.github.com>
2024-09-29 14:57:44 +0300

    Update Program.cs

--- TCP_Client/Program.cs
+++ TCP_Client/Program.cs
@@ -5,44 +5,58 @@ using System.Threading;
 
 class Client
 {
-    private const int Port = 8888; // създава константна целочислена променлива относно порта със стойност 8888
-    private const string ServerIp = "127.0.0.1"; // създава константна променлива тип низ, съдържаща 4-те октети на примерен логически IP адрес
+    // създаване на константна целочислена променлива относно номера на порта за връзка със сървъра
+    private const int Port = 8888;
+    // създаване на константна променлива тип низ за IP адрес на сървъра
+    private const string ServerIp = "127.0.0.1"; 
 
     static void Main()
     {
-        TcpClient client = new TcpClient(ServerIp, Port); // създава обект client от клас TcpClient с двете променливи - IP aдрес и номер на порта
+        // създаване на  обект client от клас TcpClient с двете променливи - IP aдрес на сървъра и номер на порта
+        TcpClient client = new TcpClient(ServerIp, Port);
         Console.WriteLine("Connected to server. Start chatting!");
 
+        // задаване на обекта stream 
         NetworkStream stream = client.GetStream();
 
+        // създаване на нова нишка за получавнане на съобщения от сървъра 
         Thread receiveThread = new Thread(ReceiveMessages);
+        // предаване на потока към нишката за получаване на съобщения 
         receiveThread.Start(stream);
 
         while (true)
         {
             string message = Console.ReadLine();
+            // преобразуване на съобщението в масив от байтове чрез ASCII кодиране
             byte[] buffer = Encoding.ASCII.GetBytes(message);
+            // записване на масива от байтове в потока към сървъра
             stream.Write(buffer, 0, buffer.Length);
         }
     }
 
+    // дефиниране на метод за получаване на съобщения от сървъра
     static void ReceiveMessages(object obj)
     {
-        NetworkStream stream = (NetworkStream)obj; // създава обект stream от клас NetwotkStream като го каства към същия тип
-        byte[] buffer = new byte[1024]; // инициализира се буферен масив от байтове с дължина 1024 
-        int bytesRead; // инициализира целочислена променлива отностно прочетените байтове
+        // преобразуване/кастване  на обекта към ъм същия тип NetwotkStream
+        NetworkStream stream = (NetworkStream)obj;
+        // създаване на буферен масив от байтове с дължина 1024
+        byte[] buffer = new byte[1024];
+        // инициализиране на целочислена променлива, съхраняваща броя прочетени байтове
+        int bytesRead; 
 
         // прави проверки за наличието на изключения (exceptions)
         while (true)
         {
             try
             {
-                bytesRead = stream.Read(buffer, 0, buffer.Length); //променливата за прочетените байтове получава стойност; взема се от буферния масив от 0 до дължината му
+                //четене на данни в буфера
+                bytesRead = stream.Read(buffer, 0, buffer.Length); 
                 if (bytesRead == 0)
                 {
                     break;
                 }
-
+                
+                // преобразуване на байтовете в низ чрез ASCII кодиране
                 string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
                 Console.WriteLine(message);
             }
